c14dbe9405342dd4cc80590531b837f554e1de7b
Atipico1 <athjk3@snu.ac.kr>
2024-01-24 16:12:34 +0900

    eval code 수정

--- eval.py
+++ eval.py
@@ -9,7 +9,15 @@ from tqdm.auto import tqdm
 import torch, wandb
 import argparse
 import pandas as pd
+import time
 from dataset import make_case_text, make_custom_case_text
+
+NAME_TO_PATH = {
+    "NQ": "Atipico1/NQ_preprocessed_with_o-u_case",
+    "TQA": "Atipico1/trivia-top5_preprocessed_with_o-u_case",
+    "WEBQ": "Atipico1/webq-top5_preprocessed_with_o-u_case"
+    }
+PATH_TO_NAME = {v:k for k,v in NAME_TO_PATH.items()}
 
 def formatting_for_evaluation(row, args):
     ctxs = "\n".join([f"Doc {idx}: {ctx['text']}" for idx, ctx in enumerate(row["ctxs"][:args.num_contexts])])
@@ -30,67 +38,25 @@ def formatting_for_evaluation(row, args):
             text = f"### Background:\n{ctxs}\n\n ### Q: {q}\n### A:"
     return text
 
-def main(args):    
-    dataset = load_dataset(args.dataset_name, split="test")
-    if args.revision is not None:
-        config = PeftConfig.from_pretrained(args.model, revision=args.revision)
-        basemodel = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16)
-        model = PeftModel.from_pretrained(basemodel, args.model, config=config)
-    else:
-        config = PeftConfig.from_pretrained(args.model)
-        basemodel = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16)
-        model = PeftModel.from_pretrained(basemodel, args.model, config=config)
-    if args.save:
-        model_name= args.model.split("/")[-2]
-        model.push_to_hub(f"Atipico1/{model_name}")
-        raise ValueError("Model saved to hub")
-    # model = model.merge_and_unload() 
-    # if args.revision is not None:
-    #     config = PeftConfig.from_pretrained(args.model, revision=args.revision)
-    # else:
-    #     config = PeftConfig.from_pretrained(args.model)
-    # inference_model = AutoModelForCausalLM.from_pretrained(
-    #     config.base_model_name_or_path, device_map="auto"
-    # )
-    # model = PeftModel.from_pretrained(inference_model, args.model)
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-    tokenizer.pad_token = tokenizer.unk_token
-    model.eval()
-    config = GenerationConfig(
-        do_sample=False
-    )
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        generation_config=config,
-        framework="pt"
-    )
-    run_name = (args.prefix + "_") if args.prefix != "" else ""
-    run_name += args.model.split("/")[-2] if len(args.model.split("/")) > 2 else args.model.split("/")[-1]
-    if args.test:
-        dataset = dataset.shuffle(seed=42)
-        dataset = dataset.select(range(10))
-        run_name += "-test"
-    dataset = preprocess_dataset(dataset, args, "test")
+def evaluate(pipe, dataset, run_name:str, args):
     result, ems, accs, f1_scores = [], [], [], []
-
     iterator = tqdm(dataset, desc="Generating...")
-    for idx, row in enumerate(iterator):
-        prompt = formatting_for_evaluation(row, args)
-        if idx < 5:
-            print(prompt)
-            print("\n\n\n\n")
-        hasanswer = any([bool(ctx["hasanswer"]) for ctx in row["ctxs"][:args.num_contexts]])
-        output = pipe(prompt,max_new_tokens=args.max_new_tokens)[0]["generated_text"]
-        output_wo_prompt = output[len(prompt):]
-        is_em = exact_match_score(output_wo_prompt, row["answers"])
-        is_acc = int(text_has_answer(row["answers"], output_wo_prompt))
-        f1_score_ = f1_score(output_wo_prompt, row["answers"])
-        ems.append(is_em)
-        accs.append(is_acc)
-        result.append([row["question"], ", ".join(row["answers"]), prompt, output_wo_prompt, is_em, is_acc, f1_score_, hasanswer])
-        iterator.set_description(desc=f"EM : {sum(ems)/len(ems)*100:.2f} | Acc : {sum(accs)/len(accs)*100:.2f}")
+    with torch.no_grad():
+        for idx, row in enumerate(iterator):
+            prompt = formatting_for_evaluation(row, args)
+            if idx < 5:
+                print(prompt)
+                print("\n\n\n\n")
+            hasanswer = any([bool(ctx["hasanswer"]) for ctx in row["ctxs"][:args.num_contexts]])
+            output = pipe(prompt,max_new_tokens=args.max_new_tokens)[0]["generated_text"]
+            output_wo_prompt = output[len(prompt):]
+            is_em = exact_match_score(output_wo_prompt, row["answers"])
+            is_acc = int(text_has_answer(row["answers"], output_wo_prompt))
+            f1_score_ = f1_score(output_wo_prompt, row["answers"])
+            ems.append(is_em)
+            accs.append(is_acc)
+            result.append([row["question"], ", ".join(row["answers"]), prompt, output_wo_prompt, is_em, is_acc, f1_score_, hasanswer])
+            iterator.set_description(desc=f"EM : {sum(ems)/len(ems)*100:.2f} | Acc : {sum(accs)/len(accs)*100:.2f}")
     df = pd.DataFrame(result, columns=["Question", "Answers", "Prompt", "Prediction", "EM", "ACC", "F1", "hasanswer"])
     data = df[["EM", "ACC", "F1", "hasanswer"]].mean().to_dict()
     answerable_em, unanswerable_em = df[df["hasanswer"] == True]["EM"].mean(), df[df["hasanswer"] == False]["EM"].mean()
@@ -111,12 +77,57 @@ def main(args):
     wandb.log({"raw_output": table, "metrics": metric})
     wandb.finish()
 
+def main(args):
+    if args.sleep:
+        time.sleep(60)
+    config = PeftConfig.from_pretrained(args.model)
+    basemodel = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16)
+    model = PeftModel.from_pretrained(basemodel, args.model, config=config)
+    if args.save:
+        model_name= args.model.split("/")[-2]
+        model.push_to_hub(f"Atipico1/{model_name}")
+        raise ValueError("Model saved to hub")
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+    tokenizer.pad_token = tokenizer.unk_token
+    model.eval()
+    config = GenerationConfig(
+        do_sample=False
+    )
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        generation_config=config,
+        framework="pt"
+    )
+    model_name = args.model.split("/")[-2] if len(args.model.split("/")) > 2 else args.model.split("/")[-1]
+    if args.test:
+        dataset = dataset.shuffle(seed=42)
+        dataset = dataset.select(range(10))
+        run_name += "-test"
+    
+    if args.datasets != []:
+        for dataset_name in args.datasets:
+            dataset = load_dataset(NAME_TO_PATH[dataset_name], split="test")
+            dataset = preprocess_dataset(dataset, args, "test")
+            run_name = f"{dataset_name}-{model_name}"
+            if args.prefix:
+                run_name = f"{args.prefix}-{run_name}"
+            evaluate(pipe, dataset, run_name, args)
+    else:
+        dataset = load_dataset(args.dataset_name, split="test")
+        dataset = preprocess_dataset(dataset, args, "test")
+        dataset_name = PATH_TO_NAME[args.dataset_name]
+        run_name = f"{dataset_name}-{model_name}"
+        evaluate(pipe, dataset, run_name, args)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_name", type=str, default="Atipico1/NQ")
+    parser.add_argument("--datasets", type=str,nargs="+", default=[])
     parser.add_argument("--model", type=str, default="Atipico1/NQ-base-v4")
     parser.add_argument("--prefix", type=str, default="")
-    parser.add_argument("--revision", type=str, default=None)
     parser.add_argument("--save", type=str2bool, default=False)
     parser.add_argument("--unanswerable", type=str2bool, default=False)
     parser.add_argument("--cbr", type=str2bool, default=False)
@@ -124,6 +135,7 @@ if __name__ == "__main__":
     parser.add_argument("--cbr_unans", type=int, default=0)
     parser.add_argument("--cbr_adv", type=int, default=0)
     parser.add_argument("--cbr_conflict", type=int, default=0)
+    parser.add_argument("--sleep", type=str2bool, default=False)
     parser.add_argument("--num_contexts", type=int, default=5)
     parser.add_argument("--custom_loss", type=str2bool, default=False)
     parser.add_argument("--max_new_tokens", type=int, default=10)
--- eval.sh
+++ eval.sh
@@ -2,62 +2,20 @@
 #SBATCH -J rag-eval
 #SBATCH -p gpu
 #SBATCH --nodes=1
-#SBATCH --nodelist=n02
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:6
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:1
 #SBATCH --mem=100G
 #SBATCH -o log/eval.out
 #SBATCH -e log/eval.err
 #SBATCH --time 72:00:00
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=1
 
 python eval.py \
- --dataset_name Atipico1/NQ-10k_preprocessed_with_o-u_case \
- --model /data/seongilpark/checkpoints/NQ-cbr-v2/checkpoint-936 \
- --num_contexts 5 \
- --unanswerable False \
+ --datasets NQ TQA WEBQ \
+ --model /data/seongilpark/checkpoints/NQ-cbr-unans-final/checkpoint-936 \
  --cbr True \
- --cbr_original 3 \
- --cbr_unans 0 \
- --prefix train:NQ-test:NQ \
- --custom_loss True \
- --test False \
- --save True
-
-python eval.py \
- --dataset_name Atipico1/NQ-10k_preprocessed_with_o-u_case \
- --model /data/seongilpark/checkpoints/NQ-base-new-hp/checkpoint-936 \
- --num_contexts 5 \
- --unanswerable False \
- --cbr True \
- --cbr_original 3 \
- --cbr_unans 0 \
- --prefix train:NQ-test:NQ \
- --custom_loss True \
- --test False \
- --save True
-
-python eval.py \
- --dataset_name Atipico1/NQ-10k_preprocessed_with_o-u_case \
- --model /data/seongilpark/checkpoints/NQ-base-unans/checkpoint-936 \
- --num_contexts 5 \
- --unanswerable False \
- --cbr True \
- --cbr_original 3 \
- --cbr_unans 0 \
- --prefix train:NQ-test:NQ \
- --custom_loss True \
- --test False \
- --save True
-# python eval.py \
-#  --dataset_name Atipico1/popQA_preprocessed_unans_with_o-u_case \
-#  --model /data/seongilpark/checkpoints//NQ-cbr-v2-custom-loss/checkpoint-936 \
-#  --num_contexts 5 \
-#  --unanswerable False \
-#  --cbr True \
-#  --cbr_original 3 \
-#  --cbr_unans 0 \
-#  --custom_loss True \
-#  --prefix train:NQ-test:popQA \
-#  --test False
+ --unanswerable True \
+ --cbr_original 1 \
+ --cbr_unans 2 \
+ --prefix 2unans
